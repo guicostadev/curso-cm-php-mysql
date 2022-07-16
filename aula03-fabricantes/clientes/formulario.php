943c1bdd7864921cd8882b0995483914ee1df908
Guilherme <guicostadev@gmail.com>
2022-07-16 11:18:08 -0300

    #Update: Fabricantes
    - Modelagem de produtos e fabricantes
    - CRUD Fabricantes

---
+++ aula03-fabricantes/clientes/formulario.php
@@ -0,0 +1,68 @@
+<?php
+
+    require '../database.php';
+
+    $id = 0;
+    if(array_key_exists('id', $_GET))
+        $id = intval($_GET['id']);
+
+    $cliente = null;
+    if($id > 0) 
+    {
+       $cliente = consultarClientes($id);
+    }
+?>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="itsguicosta">
+    <title>Curso-CM - Clientes</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/project.css" rel="stylesheet" />
+</head>
+<body class="d-flex flex-column h-100"> 
+    <?php require '../header.php'; ?>
+
+    <main class="flex-shrink-0">
+        <div class="container">
+            <?php if($cliente == null):?>
+                <h1 class="mt-5">Cadastro de Clientes</h1>
+            <?php else: ?>
+                <h1 class="mt-5">Alteração de Clientes</h1>
+            <?php endif;?>
+
+            <form class="form" action="salvar_cliente.php" method="POST">
+                <div class="">
+                    <label>Nome</label>
+                    <input type="text" class="form-control" 
+                        name="nome" value="<?= $cliente == null ? "" : $cliente['nome'];?>"  />
+                </div>
+                <div class="">
+                    <label>Telefone</label>
+                    <input type="text" class="form-control" 
+                        name="telefone" value="<?= $cliente == null ? "" : $cliente['telefone'];?>"  />
+                </div>
+                <div class="">
+                    <label>Endereço</label>
+                    <input type="text" class="form-control" 
+                        name="endereco" value="<?= $cliente == null ? "" : $cliente['endereco'];?>" />
+                </div>
+                <div class="">
+                    <?php if($cliente != null):?>
+                        <input type="hidden" name="id" value="<?php echo $cliente['id'] ?>" />
+                    <?php endif; ?>
+                    <input type="submit" class="btn btn-primary" value="Enviar" />
+                </div>
+            </form>
+        </div>
+    </main>
+
+    <?php require '../footer.php' ; ?>
+    
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js" ></script>
+</body>
+</html>
--- aula03-fabricantes/fabricantes/index.php
+++ aula03-fabricantes/fabricantes/index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="itsguicosta">
-    <title>Curso-CM - Clientes</title>
+    <title>Curso-CM - Fabricantes</title>
 
     <!-- Bootstrap core CSS -->
     <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
@@ -19,35 +19,31 @@
 
     <main class="flex-shrink-0">
         <div class="container">
-            <h1 class="mt-5">Clientes do Sistema</h1>
+            <h1 class="mt-5">Fabricantes do Sistema</h1>
             <div class="table-responsive">
                 <div class="my-2">
                     <a href="formulario.php" class="btn btn-primary">
-                        Novo Cliente
+                        Novo Fabricante
                     </a>
                 </div>
                 <table class="table table-sm table-hover">
                     <thead>
                        <th class="text-center">ID</th> 
                        <th class="text-left">Nome</th> 
-                       <th class="text-left">Telefone</th> 
-                       <th class="text-left">Endereço</th> 
                        <th class="text-center">Ações</th> 
                     </thead>
                     <tbody>
-                        <?php $clientes = listarClientes(); ?>
-                        <?php if(count($clientes) > 0): ?>
-                            <?php foreach($clientes as $cliente): ?>
+                        <?php $fabricantes = listarFabricantes(); ?>
+                        <?php if(count($fabricantes) > 0): ?>
+                            <?php foreach($fabricantes as $fabricante): ?>
                                 <tr>
-                                    <td class="text-center"><?php echo $cliente['id']; ?></td>
-                                    <td class="text-left"><?php echo $cliente['nome']; ?></td>
-                                    <td class="text-left"><?= $cliente['telefone']; ?></td>
-                                    <td class="text-left"><?= $cliente['endereco']; ?></td>
+                                    <td class="text-center"><?php echo $fabricante['id']; ?></td>
+                                    <td class="text-left"><?php echo $fabricante['nome']; ?></td>
                                     <td class="text-center">
-                                        <a href="formulario.php?id=<?= $cliente['id']?>" class="btn btn-primary">
+                                        <a href="formulario.php?id=<?= $fabricante['id']?>" class="btn btn-primary">
                                             Alterar
                                         </a>
-                                        <a href="excluir_cliente.php?id=<?= $cliente['id']?>" class="btn btn-danger ml-2">
+                                        <a href="excluir_fabricante.php?id=<?= $fabricante['id']?>" class="btn btn-danger ml-2">
                                             Excluir
                                         </a>
                                     </td>
